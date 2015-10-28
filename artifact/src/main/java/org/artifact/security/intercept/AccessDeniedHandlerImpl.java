6e849d0d5eb204cb033504b7d3777f3c4e82cee5
fu_leisen <fu_leisen@cdv.com>
2015-10-28 13:41:36 +0800

    添加权限不足的Handler

---
+++ artifact/src/main/java/org/artifact/security/intercept/AccessDeniedHandlerImpl.java
@@ -0,0 +1,37 @@
+package org.artifact.security.intercept;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.artifact.base.util.JsonResult;
+import org.artifact.base.util.JsonUtil;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * 权限拒绝Handler
+ * <p>
+ * 日期：2015年10月28日
+ * 
+ * @version 0.1
+ * @author Netbug
+ */
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+
+	public void handle(HttpServletRequest request,
+			HttpServletResponse response,
+			AccessDeniedException accessDeniedException) throws IOException,
+			ServletException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json;charset=UTF-8");
+		JsonResult result = JsonResult.ACCESS_ERROR_RESULT;
+		response.getWriter().write(JsonUtil.stringify(result));
+		response.getWriter().flush();
+	}
+
+}
